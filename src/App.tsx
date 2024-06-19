03a6641457271476e689abf8020cdf0e68e1a3a4
Martin <skripkinmartin@gmail.com>
2024-06-19 08:57:23 +0300

    Adding new heading

--- src/App.tsx
+++ src/App.tsx
@@ -102,7 +102,7 @@ const App = () => {
 
         <div className={classes.App}>
 
-            <h1>git reset --soft ORIG_HEAD</h1>
+            <h1>Git reset amend</h1>
 
             <h1>FIX branch</h1>
 
